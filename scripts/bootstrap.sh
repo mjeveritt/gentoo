de7465b23e75eafde9670eff2de941f7392438dd
M. J. Everitt <m.j.everitt@iee.org>
2018-08-19 10:27:19 +0100

    bootstrap.sh: Fix copyright messages for 2018

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Copyright 1999-2014 Gentoo Foundation
+# Copyright 1999-2018 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
 
 # people who were here:
@@ -55,11 +55,12 @@ v_echo() {
 	env "$@"
 }
 
-cvsver="$Id$" # TODO: FIXME for Git era
-cvsver=${cvsver##*,v }
-cvsver=${cvsver%%Exp*}
-cvsyear=${cvsver#* }
-cvsyear=${cvsyear%%/*}
+#cvsver="$Id$" # TODO: FIXME for Git era
+#cvsver=${cvsver##*,v }
+#cvsver=${cvsver%%Exp*}
+#cvsyear=${cvsver#* }
+#cvsyear=${cvsyear%%/*}
+cvsyear="2018"
 
 usage() {
 	echo -e "Usage: ${HILITE}${0##*/}${NORMAL} ${GOOD}[options]${NORMAL}"
